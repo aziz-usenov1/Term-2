8d131221b2b5de03cebf17f06abdaefda9ce4631
aziz-usenov1 <aziz.usenov1@gmail.com>
2024-11-14 16:14:46 +0100

    Update chunking.py

--- MongoDB import/chunking.py
+++ MongoDB import/chunking.py
@@ -21,7 +21,7 @@ print("JSON file has been split into 4 parts.")
 
 
 # Connect to MongoDB Atlas
-client = MongoClient("mongodb+srv://azizusenov1:de_1_term_2@cluster0.osgak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("mongodb+srv://azizusenov1:<password>@cluster0.osgak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client['linkedin_companies']
 collection = db['companies']
 
