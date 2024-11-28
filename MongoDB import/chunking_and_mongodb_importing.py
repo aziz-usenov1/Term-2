30dddc74a625047ce8774c405310671c700ff990
Azizbek Ussenov <aziz.usenov1@gmail.com>
2024-11-29 00:43:59 +0100

    Update chunking_and_mongodb_importing.py

--- MongoDB import/chunking_and_mongodb_importing.py
+++ MongoDB import/chunking_and_mongodb_importing.py
@@ -1,4 +1,4 @@
-# Install 'pip install pymongo'https://github.com/aziz-usenov1/Term-2/blob/main/chunking.py
+# Install 'pip install pymongo'
 
 from pymongo import MongoClient
 import json
@@ -21,23 +21,36 @@ print("JSON file has been split into 4 parts.")
 
 
 # Connect to MongoDB Atlas
-client = MongoClient("mongodb+srv://azizusenov1:<password>@cluster0.osgak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("mongodb+srv://azizusenov1:de_1_term_2@cluster0.osgak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client['linkedin_companies']
-collection = db['companies']
+collection_companies = db['companies']
+collection_skills = db['skills']
 
 # Upload JSON data
+
+# Single JSON to collection
+def upload_single_file(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    collection_skills.insert_many(data)
+    print(f"Single JSON file {filename} has been uploaded to MongoDB.")
+
+filename = 'skills.json'
+upload_single_file(filename)
+
+# Multiple JSON to collection
 def upload_multiple_files(file_list):
     for filename in file_list:
         with open(filename, 'r') as f:
             data = json.load(f)
-        collection.insert_many(data)
+        collection_companies.insert_many(data)
         print(f"Data from {filename} has been uploaded to MongoDB.")
 
 file_list = [
     'companies_part1.json',
     'companies_part2.json',
     'companies_part3.json',
-    'companies_part4.json'
+    'companies_part4.json',
 ]
 
 upload_multiple_files(file_list)
