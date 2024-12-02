57ae6275544d1cf76a7346639179b785aaa91655
Azizbek Ussenov <aziz.usenov1@gmail.com>
2024-12-02 17:24:38 +0100

    Update converter.py

--- MongoDB import/converter.py
+++ MongoDB import/converter.py
@@ -4,6 +4,7 @@ import json
 import csv
 import pandas as pd
 
+#================================================================#
 # Converting Companies.csv to Json file
 
 with open('companies.csv', 'r') as f:
@@ -18,7 +19,7 @@ with open('companies.csv', 'r') as f:
 with open('companies.json', 'w') as f:
     json.dump(data, f, indent=4)
  
-
+#================================================================#
 # Dropping all NA values in 'skills_desc' column
  
 posts = pd.read_csv('postings.csv')
@@ -26,7 +27,7 @@ posts['skills_desc'].isna().sum()
 posts.dropna(subset=['skills_desc'], inplace=True)
 posts.to_csv('skills.csv') 
 
-# Converting Postings.csv to Json file
+# Converting skills description from Postings.csv to Json file
 
 with open('skills.csv', 'r') as f:
     reader = csv.reader(f)
@@ -38,4 +39,19 @@ with open('skills.csv', 'r') as f:
              'skills_desc': row[22]})
 
 with open('skills.json', 'w') as f:
+    json.dump(data, f, indent=4)
+
+#================================================================#
+# Converting posting job description from Postings.csv to Json file
+
+with open('postings.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    data = []
+    for row in reader:
+        data.append(
+            {'job_id': row[0],
+             'description': row[3]})
+
+with open('post_description.json', 'w') as f:
     json.dump(data, f, indent=4)
