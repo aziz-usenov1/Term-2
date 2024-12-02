2ea6054c262796634ac63b4d01881ccf46fe46ca
Azizbek Ussenov <aziz.usenov1@gmail.com>
2024-12-02 17:25:34 +0100

    Update chunking_and_mongodb_importing.py

--- MongoDB import/chunking_and_mongodb_importing.py
+++ MongoDB import/chunking_and_mongodb_importing.py
@@ -3,7 +3,7 @@
 from pymongo import MongoClient
 import json
 
-# chunking json file into 4
+# chunking companies description json file into 4
 with open('companies.json', 'r') as f:
     data = json.load(f)
 
@@ -19,6 +19,24 @@ for idx, chunk in enumerate(chunks):
 
 print("JSON file has been split into 4 parts.")
 
+#============================================================================#
+# chunking postings description json file into 40
+with open('post_description.json', 'r') as f:
+    data = json.load(f)
+
+# Chunk sizes
+chunk_size = len(data) // 40
+chunks = [data[i * chunk_size:(i + 1) * chunk_size] for i in range(39)]
+chunks.append(data[39 * chunk_size:])
+
+# Saving each chunk as a json file
+for idx, chunk in enumerate(chunks):
+    with open(f'post_description__part{idx + 1}.json', 'w') as f:
+        json.dump(chunk, f, indent=4)
+
+print("JSON file has been split into 40 parts.")
+
+#============================================================================#
 
 # Connect to MongoDB Atlas
 # password is hidden
@@ -26,10 +44,11 @@ client = MongoClient("mongodb+srv://azizusenov1:<password>@cluster0.osgak.mongod
 db = client['linkedin_companies']
 collection_companies = db['companies']
 collection_skills = db['skills']
+collection_description_post = db['post_description']
 
 # Upload JSON data
 
-# Single JSON to collection: skills description to be uploaded to skills collection
+# Single skills description JSON to collection
 def upload_single_file(filename):
     with open(filename, 'r') as f:
         data = json.load(f)
@@ -39,19 +58,36 @@ def upload_single_file(filename):
 filename = 'skills.json'
 upload_single_file(filename)
 
-# Multiple JSON to collection: company description to be uploaded to companies collection
-def upload_multiple_files(file_list):
+#============================================================================#
+
+# Multiple companies JSON to collection
+def upload_multiple_files_companies(file_list):
     for filename in file_list:
         with open(filename, 'r') as f:
             data = json.load(f)
         collection_companies.insert_many(data)
         print(f"Data from {filename} has been uploaded to MongoDB.")
 
-file_list = [
+file_list_companies = [
     'companies_part1.json',
     'companies_part2.json',
     'companies_part3.json',
     'companies_part4.json',
 ]
 
-upload_multiple_files(file_list)
+upload_multiple_files_companies(file_list_companies)
+
+#============================================================================#
+
+# Multiple posting description JSON to collection
+def upload_multiple_files_posts(file_list):
+    for filename in file_list:
+        with open(filename, 'r') as f:
+            data = json.load(f)
+        collection_description_post.insert_many(data)
+        print(f"Data from {filename} has been uploaded to MongoDB.")
+
+file_list_posts = [f'post_description__part{i}.json' for i in range(1, 41)]
+
+
+upload_multiple_files_posts(file_list_posts)
